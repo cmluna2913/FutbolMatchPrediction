e2a147506bc2f3363a9448195c8013d26186c142
cmluna2913 <cmluna2913@gmail.com>
2024-10-08 02:28:14 -0500

    reorganizing repo

---
+++ src/data_extraction.py
@@ -0,0 +1,11 @@
+import configparser
+import os
+import time
+import random
+import pandas as pd
+import requests
+import openpyxl
+
+from io import StringIO
+from pathlib import Path
+from bs4 import BeautifulSoup
