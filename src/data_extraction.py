fcdb69d226efc4ae098f106f264b301d12126cb3
cmluna2913 <cmluna2913@gmail.com>
2025-02-08 16:18:13 -0600

    including variables and functions related to stat types

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -4,6 +4,7 @@ import pandas as pd
 import requests
 import datetime
 import configparser
+import os
 
 from bs4 import BeautifulSoup
 from alive_progress import alive_bar
@@ -22,13 +23,40 @@ today = datetime.datetime.now()
 today = today.strftime("%Y_%m_%d")
 
 # for the main stat pages every player should have
-table_stat_types = ['summary', 'passing', 'passing_types', 'gca', 'defense', 'possession', 'misc']
+table_stat_types = ['passing', 'passing_types', 'gca', 'defense', 'possession', 'misc']
+
+table_stat_dict = {
+    'passing': ['Date', 'Day', 'Comp', 'Round', 'Venue', 'Result', 'Squad', 'Opponent', 'Start', 'Pos', 'Min', 'Total_Cmp', 
+                'Total_Att', 'Total_Cmp%', 'Total_TotDist', 'Total_PrgDist', 'Short_Cmp', 'Short_Att', 'Short_Cmp%', 
+                'Med_Cmp', 'Med_Att', 'Med_Cmp%', 'Long_Cmp', 'Long_Att', 'Long_Cmp%', 'Ast', 'xAG', 'xA', 'KP', '1/3', 
+                'PPA', 'CrsPA', 'PrgP', 'Match Report'],
+    'passing_types': ['Date', 'Day', 'Comp', 'Round', 'Venue', 'Result', 'Squad', 'Opponent', 'Start', 'Pos', 'Min', 'Att', 
+                      'PassType_Live', 'PassType_Dead', 'PassType_FK', 'PassType_TB', 'PassType_Sw', 'PassType_Crs', 'PassType_TI', 
+                      'PassType_CK', 'Corner_In', 'Corner_Out', 'Corner_Str', 'Outcome_Cmp', 'Outcome_Off', 'Outcome_Blocks', 
+                      'Match Report'],
+    'gca': ['Date', 'Day', 'Comp', 'Round', 'Venue', 'Result', 'Squad', 'Opponent', 'Start', 'Pos', 'Min', 'SCA_SCA', 
+            'SCA_PassLive', 'SCA_PassDead', 'SCA_TO', 'SCA_Sh', 'SCA_Fld', 'SCA_Def', 'GCA_GCA', 'GCA_PassLive', 'GCA_PassDead', 
+            'GCA_TO', 'GCA_Sh', 'GCA_Fld', 'GCA_Def', 'Match Report'],
+    'defense': ['Date', 'Day', 'Comp', 'Round', 'Venue', 'Result', 'Squad', 'Opponent', 'Start', 'Pos', 'Min', 'Tkl_Tkl', 
+               'Tkl_TklW', 'Tkl_Def_3rd', 'Tkl_Mid_3rd', 'Tkl_Att_3rd', 'Chall_Tkl', 'Chall_Att', 'Chall_Tkl%', 'Chall_Lost', 
+               'Blk_Blocks', 'Blk_Sh', 'Blk_Pass', 'Int', 'Tkl+Int', 'Clr', 'Err', 'Match_Report'],
+    'possession': ['Date', 'Day', 'Comp', 'Round', 'Venue', 'Result', 'Squad', 'Opponent', 'Start', 'Pos', 'Min', 
+                   'Touches_Touches', 'Touches_Def_Pen', 'Touches_Def_3rd', 'Touches_Mid_3rd', 'Touches_Att_3rd', 
+                   'Touches_Att_Pen', 'Touches_Live', 'TakeOn_Att', 'TakeOn_Succ', 'TakeOn_Succ%', 'TakeOn_Tkld', 
+                   'TakeOn_Tkld%', 'Carries_Carries', 'Carries_TotDist', 'Carries_PrgDist', 'Carries_PrgC', 'Carries_1/3', 
+                   'Carries_CPA', 'Carries_Mis', 'Carries_Dis', 'Receiving_Rec', 'Receiving_PrgR', 'Match Report'],
+    'misc': ['Date', 'Day', 'Comp', 'Round', 'Venue', 'Result', 'Squad', 'Opponent', 'Start', 'Pos', 'Min', 'Perf_CrdY', 
+             'Perf_CrdR', 'Perf_2CrdY', 'Perf_Fls', 'Perf_Fld', 'Perf_Off', 'Perf_Crs', 'Perf_Int', 'Perf_TklW', 'Perf_PKwon', 
+             'Perf_PKcon', 'Perf_OG', 'Perf_Recov', 'AerialDuel_Won', 'AerialDuel_Lost', 'AerialDuel_Won%', 'Match Report']
+    }
 
 def get_html_data(url:str, parser:str='html.parser') -> BeautifulSoup:
     '''
     Extract html data from specified url and return a bs4 object.
     Parser can be specified if needed. Default is html.parser.
     '''
+    time.sleep(int(config['other']['request_time_limit']))
+
     response = requests.get(url)
     html_content = response.text
     soup = BeautifulSoup(html_content, parser)
@@ -224,3 +252,38 @@ def save_player_htmls(players_df:pd.DataFrame, year:int):
                     failed_links.append(stat_link)
             bar()
     return (failed_indices, failed_links)
+
+def get_html_stat_file_paths(dir, stat):
+    html_files = []
+    for roots, dirs, files in os.walk(dir):
+        for file in files:
+            if file.endswith(f'_{stat}.html'):
+                html_files.append(f"{roots}\\{file}")
+    return html_files
+
+def create_df_for_stat(html_files, stat):
+    stat_df = pd.DataFrame()
+    failed_files = []
+    with alive_bar(len(html_files), force_tty=True) as bar:
+        for file in html_files:
+            try:
+                with open(file) as fp:
+                    player_name_index = len(stat) + len('_.html')
+                    player_name = file.split('\\')[-1][:-player_name_index]
+
+                    soup = BeautifulSoup(fp, 'html.parser')
+                    
+                    temp_df = pd.read_html(StringIO(str(soup)))[0]
+                    # print(len(temp_df.columns))
+                    temp_df.columns = table_stat_dict[stat]
+
+                    temp_df['PlayerName'] = player_name
+                    temp_df['FileName'] = file
+                    
+                    stat_df = pd.concat([stat_df, temp_df], ignore_index=True)
+            except:
+                failed_files.append(file)
+            bar()
+
+
+    return stat_df, failed_files
